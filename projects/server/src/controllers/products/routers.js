25a2b89f05b5ffb32dc089344574e892e11cda40
vsebas78 <76153640+vsebas78@users.noreply.github.com>
2023-09-07 22:07:40 +0700

    Pec 33 create available unit and unit conversion rules for each product (#35)
    * ADD : BE Feature create available unit and unit conversion for each product, improve relation between Product_List, Product_Unit, Product_Detail, include Product_Unit model when getProducts, error handler for product unit
    
    * FIX : Error when merge
    
    * Add : Relation between category, product_categories, and product_lists
    
    * Delete : unwanted file from handhika
    
    * UPDATE : typo in relation
    
    * UPDATE : set limit product unit max 2,  soft delete on product unit, add error handling for product unit, imrpove database for soft delete product unit
    
    * ADD : BE_Convertion from default unit into secondary unit
    
    * UPDATE : Make new folder for convert default unit into secondary unit
    
    * UPDATE : BE_Improve logic
    
    * ADD : BE_Reactivate deleted unit, improve logic when getProduct include their units

--- projects/server/src/controllers/products/routers.js
+++ projects/server/src/controllers/products/routers.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const product = require("./index.js")
-const convertion = require("./convertion/index.js")
+const unit = require("./units/index.js")
+const convertion = require("./units/convertion/index.js")
 const {verifyUser, verifyAdmin} = require("../../middleware/token.verify.js")
 const { helperUploader } = require("../../helper/index.js");
 
@@ -13,10 +14,12 @@ router.get("/", product.getProducts)
 router.post("/", uploader.single("file"), product.createProduct) //NOTE: verifyAdmin
 router.patch("/:id", uploader.single("file"), product.updateProduct) //NOTE: verifyAdmin
 router.patch("/delete/:id", product.deleteProduct) //NOTE: verifyAdmin
-router.get("/unit", verifyAdmin, convertion.productUnits)
-router.patch("/unit/update/:productId", verifyAdmin, convertion.updateProductUnits)
-router.delete("/unit/delete/:productId", verifyAdmin, convertion.deleteProductUnits)
-router.post("/unit/:productId", verifyAdmin, convertion.setProductUnits)
+router.get("/unit", verifyAdmin, unit.productUnits)
+router.patch("/unit/update/:productId", verifyAdmin, unit.updateProductUnits)
+router.patch("/unit/delete/:productId", verifyAdmin, unit.deleteProductUnits)
+router.patch("/unit/make-convertion", convertion.makeConvertionUnit)
+router.patch("/unit/reactivate", unit.activateDeletedUnits)
+router.post("/unit/:productId", verifyAdmin, unit.setProductUnits)
 router.patch("/stock/update",  verifyAdmin, product.updateMainStock)
 
 
