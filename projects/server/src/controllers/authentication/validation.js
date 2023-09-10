07b28af03259e1b44088b6d71fb233715d5df10e
welkito <tiowellington458@gmail.com>
2023-09-10 17:22:55 +0700

    FIX : fixing bug for verify and add address API from rajaongkir

--- projects/server/src/controllers/authentication/validation.js
+++ projects/server/src/controllers/authentication/validation.js
@@ -37,7 +37,8 @@ const VerifyValidationSchema = Yup.object({
     province : Yup.string().required("Province is required"),
     city : Yup.string().required("City is required") ,
     district : Yup.string().required("District is required") ,
-    postalCode : Yup.number().required("Postal Code is required")
+    postalCode : Yup.number().required("Postal Code is required"),
+    costId : Yup.number().required("CostId is required")
 })
 
 module.exports = {
