9089059724faf3ced2b84bffac7198d387679ac2
welkito <tiowellington458@gmail.com>
2023-10-11 01:16:01 +0700

    fix : fix major bug in custom product

--- projects/client/src/pages/admin/qna/index.js
+++ projects/client/src/pages/admin/qna/index.js
@@ -6,7 +6,7 @@ import Input from "../../../components/Input/index.js"
 import {deleteQuestion, PostAnswer, getForum,getUnanswered } from "../../../store/slices/forum/slices.js"
 import {formatDate} from "../../../utils/formatDate.js" 
 import Button from "../../../components/Button/index.js"
-import Pagination from "../../../components/Pagination/index.js"
+import Pagination from "../../../components/PaginationV2/index.js"
 import Modal from "../../../components/Modal/index.js"
 import Message from "../../../components/Message/index.js"
 import { FaSortAlphaDown, FaSortAlphaUp } from "react-icons/fa"
@@ -115,44 +115,44 @@ function QNA () {
             }, 2000)
         }
     }
-useEffect(async ()=>{
+useEffect( ()=>{
     dispatch(getUnanswered(({sortDate : (sortDate ? "DESC" : "ASC")})))
 
-        setUnansweredList([])
-        setAnsweredList([])
-        const answerlist = []
-        const unanswerlist = []
-        questionList.map(item=>{
-            if(item.answer){
-                answerlist.push(item)
-            }
-            else{
-                unanswerlist.push(item)
-            }
-        })
-        setAnsweredList(answerlist)
-        setUnansweredList(unanswerlist)
+        // setUnansweredList([])
+        // setAnsweredList([])
+        // const answerlist = []
+        // const unanswerlist = []
+        // questionList.map(item=>{
+        //     if(item.answer){
+        //         answerlist.push(item)
+        //     }
+        //     else{
+        //         unanswerlist.push(item)
+        //     }
+        // })
+        // setAnsweredList(answerlist)
+        // setUnansweredList(unanswerlist)
     
 },[])
 
     useEffect(() => {
         dispatch(getUnanswered(({sortDate : (sortDate ? "DESC" : "ASC")})))
-        if(questionList){
-            setUnansweredList([])
-            setAnsweredList([])
-            const answerlist = []
-            const unanswerlist = []
-            questionList.map(item=>{
-                if(item.answer){
-                    answerlist.push(item)
-                }
-                else{
-                    unanswerlist.push(item)
-                }
-            })
-            setAnsweredList(answerlist)
-            setUnansweredList(unanswerlist)
-        }
+        // if(questionList){
+        //     setUnansweredList([])
+        //     setAnsweredList([])
+        //     const answerlist = []
+        //     const unanswerlist = []
+        //     questionList.map(item=>{
+        //         if(item.answer){
+        //             answerlist.push(item)
+        //         }
+        //         else{
+        //             unanswerlist.push(item)
+        //         }
+        //     })
+        //     setAnsweredList(answerlist)
+        //     setUnansweredList(unanswerlist)
+        // }
     }, [sortDate])
 
 
--- projects/server/src/controllers/products/recipe/index.js
+++ projects/server/src/controllers/products/recipe/index.js
@@ -1,7 +1,7 @@
 const {middlewareErrorHandling} = require("../../../middleware/index.js");
 const {Product_Recipe, Product_List, Product_Unit, Product_Detail, Product_History, 
   Transaction_List, Transaction_Detail} = require("../../../model/relation.js")
-  const moment = require("moment")
+// const moment = require("moment")
 const {Op} = require("sequelize")
 const path = require("path")
 const fs = require("fs")
@@ -29,14 +29,16 @@ const getUser = async( req, res, next ) => {
     const currentPage = page ? parseInt(page) : 1;
 
     const options = {
-        offset : currentPage > 1 ? parseInt(currentPage-1)*10 : 0,
-        limit : 10,
+        offset : currentPage > 1 ? parseInt(currentPage-1)*6 : 0,
+        limit : 6,
     }
 
     const filter = {}
     const sort =  [[`createdRecipe`, sortDate ? sortDate : "DESC"]]
 
-    if(search) filter.name= {[Op.like]: `%${search}%`}
+    // if(search) filter = {
+    //   name :{[Op.like]: `%${search}%`} 
+    // }
     console.log(filter)
     const userlist = await User_Account.findAll({...options,
         where : {
@@ -47,9 +49,9 @@ const getUser = async( req, res, next ) => {
         include : [{
           model : User_Profile,
           as : "userProfile",
-          where : 
-            filter?.name
-          
+          where : {
+            name :{[Op.like]: `%${search}%`} 
+          }
         }],
         order : sort
     })
@@ -61,10 +63,9 @@ const getUser = async( req, res, next ) => {
   },include : {
     model : User_Profile,
     as : "userProfile",
-    where : {[Op.and] :
-    [
-        filter
-    ]}
+    where : {
+      name :{[Op.like]: `%${search}%`} 
+    }
   },
     })
 
@@ -241,7 +242,11 @@ const getUser = async( req, res, next ) => {
               // if (error) throw error;
               console.log("Email sent: " + info.response);
           })
-  
+          
+      await User_Account.update({
+        imgRecipe : null
+      },{where : {email}})
+
       res.status(200).json({ 
         type : "success",
         message : "We have send the desired user",
@@ -367,7 +372,7 @@ const getUser = async( req, res, next ) => {
             productId : ingredientProductId,
             unit : secUnit.product_unit.name,
             initialStock : secUnit?.quantity + remainStock,
-            status : "Penjualan",
+            status : "Penjualan Obat Racik",
             type : "Pengurangan",
             quantity : +product?.productDescription * recipeQty,
             results : remainSecStock
@@ -401,7 +406,7 @@ const getUser = async( req, res, next ) => {
           productId : ingredientProductId,
           unit : secUnit.product_unit.name,
           initialStock : secUnit?.quantity,
-          status : "Penjualan",
+          status : "Penjualan Obat Racik",
           type : "Pengurangan",
           quantity : +product?.productDescription * recipeQty,
           results : secUnit?.quantity - (+product?.productDescription * recipeQty)
@@ -493,7 +498,7 @@ const getUser = async( req, res, next ) => {
         statusId : 1,
         addressId : address?.addressId,
         expired : moment().add(1,"d").format("YYYY-MM-DD hh:mm:ss"),
-      invoice : moment().format("YYYY-MM-DD hh:mm:ss").toString()
+        invoice : moment().format("YYYY-MM-DD hh:mm:ss").toString()
       }
       const listResult = await Transaction_List.create(translist)
 
@@ -508,9 +513,11 @@ const getUser = async( req, res, next ) => {
       })
     }
 
-    // if (productResult.length > 0) {
-    //   await Transaction_Detail.bulkCreate(productResult);
-    // }
+    await User_Account.update({
+      addressIdRecipe : null,
+      createdRecipe : null,
+      shippingRecipe : null,
+    },{where :  {userId : user?.userId}})
 
       res.status(200).json({ 
         type : "success",
