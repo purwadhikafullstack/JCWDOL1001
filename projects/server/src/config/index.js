1c3099324a82777590b998e3467e858bbe079c2a
Bryan Karta <65345089+Servond@users.noreply.github.com>
2023-10-11 14:58:50 +0700

    Update index.js

--- projects/server/src/config/index.js
+++ projects/server/src/config/index.js
@@ -1,5 +1,7 @@
-const dotenv = require("dotenv")
-dotenv.config();
+const path = require("path");
+require("dotenv").config({
+    path: path.resolve(__dirname, '../../.env')
+})
 
 // @create db configuration
 const db_config = Object.freeze({
